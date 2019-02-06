9fed33a1a95fb443786be5c62ba86a5a8babcdf0
cbelloda <cbelloda@gmail.com>
2019-02-05 22:25:26 -0500

    Initial commit

---
+++ app/src/main/java/curso/belloda/com/curso_android_studio/MainActivity.java
@@ -0,0 +1,13 @@
+package curso.belloda.com.curso_android_studio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
